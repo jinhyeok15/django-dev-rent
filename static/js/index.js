52c3619f57695b669965287d7366c40b9acb122f
jinhyeok15 <ekfrhd97@gmail.com>
2022-05-21 00:40:48 +0900

    feat: delete 버튼 alert event 추가

---
+++ static/js/index.js
@@ -0,0 +1,11 @@
+const deleteSubmitForm = document.getElementById('delete-page-form');
+
+function deleteSubmitEvent(event) {
+    event.preventDefault();
+    const answer = confirm("삭제하시겠습니까?");
+    if (answer===true) {
+        deleteSubmitForm.submit();
+    }
+}
+
+deleteSubmitForm.addEventListener('submit', deleteSubmitEvent);
